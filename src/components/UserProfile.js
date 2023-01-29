eed4f4004da5e624d088f22044bbf6f487b36fc4
muditsharma01 <muditshar001@gmail.com>
2023-01-29 18:48:37 +0530

    Update UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,15 @@
-import { useContext } from 'react'
-import React from 'react'
+import React, { useContext } from 'react'
 import { UserContext } from "./App"
 
-const UserProfile = () =>{
+const UserProfile = () => {
+  const userData = useContext(UserContext);
 
+  return (
+    <div>
+      <h1 id="name">Name: {userData.name}</h1>
+      <h1 id="age">Age: {userData.age}</h1>
+    </div>
+  )
 }
 
-export {UserProfile}
+export { UserProfile };
